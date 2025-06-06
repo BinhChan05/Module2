cc225d8d1726873b2833fcb984c811c4ae3e8f2d
BinhChan05 <tranbinh110905@gmail.com>
2025-06-06 19:02:06 +0700

    mmm

---
+++ src/ss5/Test.java
@@ -0,0 +1,11 @@
+package ss5;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setClasses("23NS1");
+        s.setName("Jack");
+        System.out.println(s.getName());
+        System.out.println(s.getClasses());
+    }
+}
